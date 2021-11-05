5ba21b9b3aff90d74f9a2922dae0d9231759f224
Helena <helenalundstrom@hotmail.se>
2021-11-05 17:03:34 +0100

    Added test getting dimensions 3x3

--- src/test/java/se/gameoflife/GameLoaderTest.java
+++ src/test/java/se/gameoflife/GameLoaderTest.java
@@ -10,11 +10,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GameLoaderTest {
 
     @Test
-    void callingReadFirstLineSetsDimensionsToOneOne() {
+    void callingLoadGameReturnsDimensionsToOneOne() {
         GameLoader gameLoader = new GameLoader();
         List<String> strings = new ArrayList<>(List.of("1,1"));
         int[] expected = {1,1};
         int[] dimensions = gameLoader.loadGame(strings);
         assertThat(dimensions).isEqualTo(expected);
     }
+
+    @Test
+    void callingLoadGameReturnsDimensionsToThreeThree() {
+        GameLoader gameLoader = new GameLoader();
+        List<String> strings = new ArrayList<>(List.of("3,3"));
+        int[] expected = {3,3 };
+        int[] dimensions = gameLoader.loadGame(strings);
+        assertThat(dimensions).isEqualTo(expected);
+    }
+
+
 }
