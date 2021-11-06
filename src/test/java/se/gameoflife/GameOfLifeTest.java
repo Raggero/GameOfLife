440e709aa880371e864a590747deaf78a5ad5a2c
Helena <helenalundstrom@hotmail.se>
2021-11-06 12:06:05 +0100

    Added test for calling readfile with correct argument

--- src/test/java/se/gameoflife/GameOfLifeTest.java
+++ src/test/java/se/gameoflife/GameOfLifeTest.java
@@ -3,10 +3,12 @@ package se.gameoflife;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -28,6 +30,18 @@ public class GameOfLifeTest {
         when(mockedFileReader.readFile(Mockito.anyString())).thenReturn(Mockito.any());
         gameOfLife.startGame();
         verify(mockedFileReader).readFile(Mockito.anyString());
+    }
+
+    @Test
+    void callingStartGameCallsReadFileWithCorrectArgument() {
+        String path = "src/main/resources/gameoflife.txt";
+        when(mockedFileReader.readFile(path)).thenReturn(Mockito.any());
+        gameOfLife.startGame();
+
+        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+        verify(mockedFileReader).readFile(stringCaptor.capture());
+        String capturedArgument = stringCaptor.getValue();
+        assertThat(capturedArgument).isEqualTo(path);
     }
 
 
