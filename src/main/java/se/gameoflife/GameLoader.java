46f45d71c6dab225ff6fd76e210963ec202b011e
Helena <helenalundstrom@hotmail.se>
2021-11-06 11:18:53 +0100

    Added dimensions as field so all methods can use them. updated test that method, load dimensions now sets that field. Willreturns void

--- src/main/java/se/gameoflife/GameLoader.java
+++ src/main/java/se/gameoflife/GameLoader.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class GameLoader {
 
-    public int[] loadDimensions(List<String> stringList) {
-        String[] dimensions = stringList.get(0).split(",");
-        return Arrays.stream(dimensions)
+    int[] dimensions;
+
+    public void loadDimensions(List<String> stringList) {
+        String[] stringDimenstions = stringList.get(0).split(",");
+        Arrays.stream(stringDimenstions)
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
@@ -15,8 +17,13 @@ public class GameLoader {
 
     public Board loadBoard(List<String> stringList) {
         Board board = new Board();
-        if(stringList.get(1).equals("x")){
-            board.add(new Cell(0,0));
+        for (int i = 0; i < dimensions[0]; i++) {
+            String[] cell = stringList.get(i + 1).trim().split(" ");
+            for (int j = 0; j < dimensions[i]; j++) {
+                if(cell[j].equals("x")){
+                    board.add(new Cell(i,j));
+                }
+            }
         }
         return board;
     }
--- src/test/java/se/gameoflife/GameLoaderTest.java
+++ src/test/java/se/gameoflife/GameLoaderTest.java
@@ -14,8 +14,8 @@ public class GameLoaderTest {
         GameLoader gameLoader = new GameLoader();
         List<String> strings = new ArrayList<>(List.of("1,1"));
         int[] expected = {1,1};
-        int[] dimensions = gameLoader.loadDimensions(strings);
-        assertThat(dimensions).isEqualTo(expected);
+        gameLoader.loadDimensions(strings);
+        assertThat(gameLoader.dimensions).isEqualTo(expected);
     }
 
     @Test
@@ -23,8 +23,8 @@ public class GameLoaderTest {
         GameLoader gameLoader = new GameLoader();
         List<String> strings = new ArrayList<>(List.of("3,3"));
         int[] expected = {3,3 };
-        int[] dimensions = gameLoader.loadDimensions(strings);
-        assertThat(dimensions).isEqualTo(expected);
+        gameLoader.loadDimensions(strings);
+        assertThat(gameLoader.dimensions).isEqualTo(expected);
     }
 
     @Test
@@ -42,6 +42,15 @@ public class GameLoaderTest {
         List<String> strings = new ArrayList<>(List.of("1,1", "x"));
         Board board = gameLoader.loadBoard(strings);
         Board expected = new Board(new ArrayList<>(List.of(new Cell(0,0))));
+        assertThat(board).isEqualTo(expected);
+    }
+
+    @Test
+    void callingLoadBoardWithTwoAliveCellsWithOneDeadInBetweenReturnsBoardWithTwoCellInList() {
+        GameLoader gameLoader = new GameLoader();
+        List<String> strings = new ArrayList<>(List.of("1,3", "x . x"));
+        Board board = gameLoader.loadBoard(strings);
+        Board expected = new Board(new ArrayList<>(List.of(new Cell(0,0), new Cell(0,2))));
         assertThat(board).isEqualTo(expected);
     }
 
